65d8932d4aa9da073d7e30ce0142891cdebd49db
mmcdon20 <mmcdon20@live.com>
2013-11-18 16:29:55 -0600

    created function for media items, and used it on various pages

--- models/db_musicapp.py
+++ models/db_musicapp.py
@@ -26,3 +26,39 @@ def fullname(user_id):
     if user_id is None:
         return "Unknown"
     return "%(first_name)s %(last_name)s" % db.auth_user(user_id)
+
+def musicitem(post):
+    if post is None:
+        return None
+    
+    name        = fullname(post.created_by)
+    postref     = URL('post',args=post.id)
+    imageref    = URL('static', 'images/tn_placeholder.png')
+    genrelink   = str(A(post.genre, _href=URL('genre', args=post.genre)))
+    postlink    = str(A(post.title, _href=postref))
+    date        = prettydate(post.created_on)
+    userlink    = str(A(name,_href=URL('profile',args=post.created_by)))
+    description = post.description
+    attachref   = URL('download',args=post.attachment)
+    
+    return XML("""
+    <div class="row">
+        <div class="span12 post-container">
+            <ul class="media-list">
+                <li class="media">
+                    <a class="pull-left" href=\"""" + postref + """">
+                        <img class="media-object" src=\"""" + imageref + """">
+                    </a>
+                    <div class="media-body">               
+                        <h4 >""" + genrelink + " / " + postlink + """</h4>
+                        <h5>""" + date + " | Posted by " + userlink + """</h5>
+                        <p>""" + description + """</p>
+                        <audio controls="controls" style="max-width:100%">
+                            <source src=\"""" + attachref + """">
+                        </audio>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+    """)
