eb0e9cafc35621c499ce16dc3cfb99a2dc8b5675
mmcdon20 <mmcdon20@live.com>
2013-12-08 07:04:24 -0600

    separated ajax button functions into their own controller

--- controllers/default.py
+++ controllers/default.py
@@ -174,32 +174,3 @@ def data():
       LOAD('default','data.load',args='tables',ajax=True,user_signature=True)
     """
     return dict(form=crud())
-
-def friendly():
-    
-    return locals()
-
-def addFriend():
-    db.relationship.insert(person=request.post_vars.person, status=request.post_vars.status)
-    return locals()
-
-def removeFriend():
-    db(db.relationship.id == request.post_vars.rid).delete()
-    return locals()
-
-def changeStatus():
-    id = request.post_vars.commentid
-    post = db.post(id)
-    new_status = request.post_vars.status
-    
-    record = db((db.post_like.post == id) & (db.post_like.created_by == auth.user.id)).select()
-
-    if not record:
-        db.post_like.insert(post=id, status=new_status)
-    elif record[0].status == new_status:
-        db((db.post_like.post==id) & (db.post_like.created_by==auth.user.id)).delete()
-    else:
-        db((db.post_like.post==id) & (db.post_like.created_by==auth.user.id)).update(status=new_status)
-    
-    redirect(URL('index'))
-    return locals()
